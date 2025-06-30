6984acfba3eac0291d84c8c8129c5485ebccfecb
Dmitriy Monakhov <dimamon6032403@gmail.com>
2025-06-30 14:01:34 +0200

    SetSfetPresent fixed

--- public/PiSubmarine/Bq25792/Bq25792.h
+++ public/PiSubmarine/Bq25792/Bq25792.h
@@ -300,28 +300,6 @@ namespace PiSubmarine::Bq25792
 			m_DirtyRegs[RegUtils::ToInt(RegOffset::ChargeCurrentLimit)] = true;
 		}
 
-		/*
-		* struct Reg14ChargerControl5 : RegUtils::Register<RegOffset::ChargerControl5, 1>
-		{
-			using RegUtils::Register<RegOffset::ChargerControl5, 1>::Register;
-
-			enum class IbatRegValues
-			{
-				Amps3 = 0,
-				Amps4 = 1,
-				Amps5 = 2,
-				Disable = 3
-			};
-
-			RegUtils::Field<uint8_t, 7, 1, RegUtils::Access::ReadWrite, GetSize(), std::endian::big> SfetPresent{ Data };
-			RegUtils::Field<uint8_t, 5, 1, RegUtils::Access::ReadWrite, GetSize(), std::endian::big> EnIbat{ Data };
-			RegUtils::Field<IbatRegValues, 3, 2, RegUtils::Access::ReadWrite, GetSize(), std::endian::big> IbatReg{ Data };
-			RegUtils::Field<uint8_t, 2, 1, RegUtils::Access::ReadWrite, GetSize(), std::endian::big> EnIinDpm{ Data };
-			RegUtils::Field<uint8_t, 1, 1, RegUtils::Access::ReadWrite, GetSize(), std::endian::big> EnExtLim{ Data };
-			RegUtils::Field<uint8_t, 0, 1, RegUtils::Access::ReadWrite, GetSize(), std::endian::big> EnBatoc{ Data };
-		};
-		*/
-
 		IbatReg GetOtgMaxCurrent() const
 		{
 			return RegUtils::Read<IbatReg, std::endian::big>(m_ChargerMemoryBuffer.data() + RegUtils::ToInt(RegOffset::ChargerControl5), 3, 2);
@@ -340,7 +318,7 @@ namespace PiSubmarine::Bq25792
 
 		void SetSfetPresent(bool value)
 		{
-			RegUtils::Write<IbatReg, std::endian::big>(value, m_ChargerMemoryBuffer.data() + RegUtils::ToInt(RegOffset::ChargerControl5), 7, 1);
+			RegUtils::Write<uint8_t, std::endian::big>(value, m_ChargerMemoryBuffer.data() + RegUtils::ToInt(RegOffset::ChargerControl5), 7, 1);
 			m_DirtyRegs[RegUtils::ToInt(RegOffset::ChargerControl5)] = true;
 		}
 
